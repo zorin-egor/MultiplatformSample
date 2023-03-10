f4b65da9fe9de0d77920728f77266d952058b20b
Egor Zorin <zorin.egor.apps@gmail.com>
2023-03-10 10:20:41 +0300

    Dev code

--- common/details/api/src/commonMain/kotlin/com/sample/multiplatform/models/Details.kt
+++ common/details/api/src/commonMain/kotlin/com/sample/multiplatform/models/Details.kt
@@ -2,8 +2,6 @@ package com.sample.multiplatform.models
 
 data class Details(
     val id: Long,
-    var updateTime: Long = 0,
-    var userId: Long = -1,
     var nodeId: String? = null,
     var login: String? = null,
     var url: String? = null,
